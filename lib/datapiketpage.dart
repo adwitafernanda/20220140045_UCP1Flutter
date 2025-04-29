5703a8e609b430eeb37e9dcc10b163abfeef1aa2
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 18:49:04 +0700

    ADD -  halaman DataPiket dengan parameter nama untuk menampilkan data piket

---
+++ lib/datapiketpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DataPiket extends StatefulWidget {
+  final String nama;
+  
+  const DataPiket({super.key, required this.nama});
+
+  @override
+  State<DataPiket> createState() => _DataPiketState();
+}
