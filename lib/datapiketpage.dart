58d157425bbe50cccaf6355d5b4fae86b7c6e2f1
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 18:49:47 +0700

    ADD - form input untuk tugas piket dan daftar tugas dengan pemetaan hari dan bulan dalam bahasa Indonesia

--- lib/datapiketpage.dart
+++ lib/datapiketpage.dart
@@ -8,3 +8,37 @@ class DataPiket extends StatefulWidget {
   @override
   State<DataPiket> createState() => _DataPiketState();
 }
+
+class _DataPiketState extends State<DataPiket> {
+  final _formKey = GlobalKey<FormState>();
+
+  late final TextEditingController _namaController;
+  final TextEditingController _tugasController = TextEditingController();
+  final TextEditingController _tanggalController = TextEditingController();
+
+  List<Map<String, dynamic>> _daftarTugas = [];
+
+  final Map<String, String> _indonesianDays = {
+    'Monday': 'Senin',
+    'Tuesday': 'Selasa',
+    'Wednesday': 'Rabu', 
+    'Thursday': 'Kamis',
+    'Friday': 'Jumat',
+    'Saturday': 'Sabtu',
+    'Sunday': 'Minggu',
+  };
+
+  final Map<String, String> _indonesianMonths = {
+    'January': 'Januari',
+    'February': 'Februari',
+    'March': 'Maret',
+    'April': 'April',
+    'May': 'Mei',
+    'June': 'Juni',
+    'July': 'Juli',
+    'August': 'Agustus',
+    'September': 'September',
+    'October': 'Oktober',
+    'November': 'November',
+    'December': 'Desember',
+  };
