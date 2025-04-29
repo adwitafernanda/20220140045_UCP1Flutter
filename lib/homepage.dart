709cbca484365aea842afe22eef18cd961d1b92f
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 18:44:57 +0700

    ADD - tampilan HomePage dengan sapa pengguna dan foto profil

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,3 +9,38 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 1, 1),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/images/profile.jpg'),
+              radius: 20,
+            ),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Selamat Datang',
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+                Text(
+                  widget.nama,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
