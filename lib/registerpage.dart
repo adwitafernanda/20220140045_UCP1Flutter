3905e3007ab5d265454e9a55f2590a9334af3126
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 18:33:45 +0700

    ADD - controller untuk email, password, konfirmasi password, nama, dan telepon

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -6,3 +6,16 @@ class RegisterPage extends StatefulWidget {
   @override
   State<RegisterPage> createState() => _RegisterPageState();
 }
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confpasswordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  bool _obscurePassword = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+}
