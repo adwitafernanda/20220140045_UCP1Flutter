5286bb7aa1338bde0729b86c326bbe798992fdda
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 19:07:54 +0700

    ADD - halaman detail untuk menampilkan data pelanggan

---
+++ lib/detailpelangganpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailPelangganPage extends StatelessWidget {
+  final String nama;
+  final String email;
+  final String noHp;
+  final String alamat;
+  final String provinsi;
+  final String kodePos;
+
+  const DetailPelangganPage({
+    super.key,
+    required this.nama,
+    required this.email,
+    required this.noHp,
+    required this.alamat,
+    required this.provinsi,
+    required this.kodePos,
+  });
