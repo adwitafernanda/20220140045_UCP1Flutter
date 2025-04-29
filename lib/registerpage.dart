ad0bc131439dba165aa33456b467069b98458b2a
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 18:34:44 +0700

    ADD - tampilan form registrasi dengan gambar dan teks judul

--- lib/registerpage.dart
+++ lib/registerpage.dart
@@ -18,4 +18,29 @@ class _RegisterPageState extends State<RegisterPage> {
   void initState() {
     super.initState();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logospecss.png',
+                height: 75,
+              ),
+              const SizedBox(height: 25),
+              const Text(
+                'DAFTAR AKUN BARU',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
 }
