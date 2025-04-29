b69de2c3330a1ccbef4343b6dea3e50e41caa193
adwitafernanda <adwitafernanda4@gmail.com>
2025-04-29 19:10:50 +0700

    ADD - struktur awal untuk halaman DataBarang

---
+++ lib/databarangpage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DataBarang extends StatefulWidget {
+  const DataBarang({super.key});
+
+  @override
+  State<DataBarang> createState() => _DataBarangState();
+}
